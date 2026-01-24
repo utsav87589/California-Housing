632ee20f49c3a9be770067b59bcf0d5c99f5a0fd
utsav87589 <gkundukundu987@gmail.com>
2026-01-23 21:41:12 -0500

    changed the function to properly print the statement

--- v2/supervised_ml/src/processing/process_categorical.py
+++ v2/supervised_ml/src/processing/process_categorical.py
@@ -5,4 +5,4 @@ import numpy as np
 ### function to check the nunique and the value_counts for the categorical columns
 def get_nunique_value_counts(df, col) : 
 
-    return f"{df.nunique()} \n{df.value_counts()}"
+    print(f"{df[col].nunique()} \n{df[col].value_counts()}")
