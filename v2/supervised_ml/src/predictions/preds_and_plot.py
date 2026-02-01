47de1fdb0dc124b04db7d43162ec46ee5afa5750
utsav87589 <gkundukundu987@gmail.com>
2026-02-01 12:44:31 -0500

    functions for the best model prediction, other model predcition on the valid and function to predict on the train slightly modified

--- v2/supervised_ml/src/predictions/preds_and_plot.py
+++ v2/supervised_ml/src/predictions/preds_and_plot.py
@@ -63,13 +63,70 @@ def pred_train_model(X_train, X_test, y_train, y_test, model, model_path, scaler
         plot_metrices(y_pred, y_test)
         plot_graphs(y_test, y_pred)
 
-    model.fit(X_train, y_train)
-    y_pred = model.predict(X_test)
+    else : 
+        model.fit(X_train, y_train)
+        y_pred = model.predict(X_test)
 
-    #-------------saving the model
-    joblib.dump(model, model_path)
+        #-------------saving the model
+        joblib.dump(model, model_path)
 
-    #---------plotting
-    plot_metrices(y_pred, y_test)
-    plot_graphs(y_test, y_pred)
+        #---------plotting
+        plot_metrices(y_test, y_pred)
+        plot_graphs(y_test, y_pred)
+
+
+### function to predict on the valid for the best model from tree and distance based models
+
+def best_model_predict(df, best_model_path, scaler_price_path = None) : 
+
+    # -------------for the distance based models
+
+    if scaler_price_path is not None : 
+
+        scaler_price = joblib.load(scaler_price_path)
+        model = joblib.load(best_model_path)
+
+        y_pred_best_model = model.predict(df)
+        y_pred_best_model = scaler_price.inverse_transform(np.array(y_pred_best_model).reshape(-1, 1))
+
+        return y_pred_best_model
+    
+    else : 
+        # ----------this part is exclusively for the tree
+
+        model = joblib.load(best_model_path)
+
+        y_pred_best_model = model.predict(df)
+
+        return y_pred_best_model
+
+
+### function to let the other models predict on the valid data and then compare the results with best model
+def other_model_predict(df, y_pred_best_model, other_model_path, scaler_price_path = None) : 
+
+    # -------------for the distance based models
+
+    if scaler_price_path is not None : 
+
+        scaler_price = joblib.load(scaler_price_path)
+        model = joblib.load(other_model_path)
+
+        y_pred_other_model = model.predict(df)
+        y_pred_other_model = scaler_price.inverse_transform(np.array(y_pred_other_model).reshape(-1, 1))
+
+        #---------plotting
+        plot_metrices(y_pred_best_model, y_pred_other_model)
+        plot_graphs(y_pred_best_model, y_pred_other_model)
+
+    else : 
+        
+        # ----------this part is exclusively for the tree
+
+        model = joblib.load(other_model_path)
+
+        y_pred_other_model = model.predict(df)
+
+        #---------plotting
+        plot_metrices(y_pred_best_model, y_pred_other_model)
+        plot_graphs(y_pred_best_model, y_pred_other_model)
 
